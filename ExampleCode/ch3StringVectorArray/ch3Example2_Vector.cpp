5caa1c9234a875dcec04d21980f5096f54d3091e
utsav-ubuntu-mate <gutsavgupta@gmail.com>
2019-05-28 03:40:04 +0530

    adding vector example

---
+++ ExampleCode/ch3StringVectorArray/ch3Example2_Vector.cpp
@@ -0,0 +1,18 @@
+/* This example is created to understand basics
+ * of vector library in c++ and create some ex
+ * program to test it out in present code
+*/
+
+namespace tEXAMPLE2
+{ 
+    void test1()
+    {
+        // write test-1 related to vector
+    }
+}
+
+int main( int argc, const char *argv[])
+{
+    /* write the code here */
+    return 0;
+}     
