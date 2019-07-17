acae30992f6cda6b208938d4f036516ac722a778
utsav-ubuntu-mate <gutsavgupta@gmail.com>
2019-07-18 01:59:48 +0530

    class 7.4{class scope} completed

---
+++ ExampleCode/ch7Classes/ch7Example4_classScope.cpp
@@ -0,0 +1,26 @@
+/*
+ * C++ Class Scope
+ *
+ * Note: Once the class name is seen, the remainder of the definition—including 
+ *       the parameter list and the function body—is in the scope of the class. 
+ *       As a result, we can refer to other class members without qualification
+ *
+ * Note: Member function definitions are processed after the compiler processes
+ *       all of the declarations in the class.
+ *
+ * Note: Member function definitions are processed after the compiler processes 
+ *       all of the declarations in the class.
+ *
+ *       Definitions of type names usually should appear at the beginning of a class. 
+ *       That way any member that uses that type will be seen after the type name
+ *       has already been defined.
+ *
+ *       Even though the class member is hidden, it is still possible to use that
+ *       member by qualifying the member’s name with the name of its class or by
+ *       using the this pointer explicitly.
+ */
+
+int main()
+{
+    return 0;
+}
