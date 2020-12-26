3bc1f57838df06ae90272da9eb4e90f319e03300
Utsav Gupta <utsavgupta@pop-os.localdomain>
2020-12-27 02:09:32 +0530

    Introduced the clang formatter to format c++ related code files

--- ch7Classes/projectPerson/inc/Person.hpp
+++ ch7Classes/projectPerson/inc/Person.hpp
@@ -10,52 +10,46 @@
 #include <iostream>
 #include <string>
 
-namespace person{
+namespace person {
 
 class Person
 {
-    public:
-        //@constructor
-        Person() = default;
+public:
+    //@constructor
+    Person() = default;
 
-        //@object calls
-        const std::string& getName() const;
-        const std::string& getAddr() const;
-        
-        //@static calls
-        static std::istream& read(Person& obj,
-                                  std::istream& inp);
-        static std::ostream& dump(const Person& obj,
-                                  std::ostream& out);
-    private:
-        std::string _addr;
-        std::string _name;
+    //@object calls
+    const std::string& getName() const;
+    const std::string& getAddr() const;
+
+    //@static calls
+    static std::istream& read(Person& obj, std::istream& inp);
+    static std::ostream& dump(const Person& obj, std::ostream& out);
+
+private:
+    std::string _addr;
+    std::string _name;
 };
 
-inline
-const std::string& Person::getName() const
+inline const std::string& Person::getName() const
 {
     return _name;
 }
 
-inline
-const std::string& Person::getAddr() const
+inline const std::string& Person::getAddr() const
 {
     return _addr;
 }
 
-inline
-std::istream& Person::read(Person& obj, std::istream& inp)
+inline std::istream& Person::read(Person& obj, std::istream& inp)
 {
     return inp >> obj._name >> obj._addr;
 }
 
-inline
-std::ostream& Person::dump(const Person& obj, std::ostream& out)
+inline std::ostream& Person::dump(const Person& obj, std::ostream& out)
 {
-    return out << "Person name:\t" << obj._name
-               << "Person addr:\t" << obj._addr
+    return out << "Person name:\t" << obj._name << "Person addr:\t" << obj._addr
                << std::endl;
 }
 
-}// namespace Person
+} // namespace person
